16fa777b12a8c1a3d2e6651cfda4425e27fe53e9
Christy <caublecm@gmail.com>
2021-04-13 12:44:57 -0700

    Updated code to scrape multiple return pages by creating a function that takes in a page number and another that parses.  Then created a for loop that called the functions on pages 1 to 10. It then adds the results to the akc dataframe and saves the results to a .csv file

--- akc.py
+++ akc.py
@@ -7,42 +7,41 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-# Call AKC Marketplace for breeders advertising new or upcoming litters within 50mile radius of zipcode 98053
-r = requests.get('https://marketplace.akc.org/puppies?location=98053&radius=50')
-html = r.text
-parser = BeautifulSoup(html, 'html.parser')
+# Extract: Call AKC Marketplace for breeders advertising new or upcoming litters within 50mile radius of zipcode 98053
+def extract(pnum):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
+    r = requests.get(f'https://marketplace.akc.org/puppies?location=98053&page={pnum}&radius=50', headers)
+    html = r.content
+    parser = BeautifulSoup(html, 'html.parser')
+    return parser
 
-# Isolates the breeder, breed and litter information from the html which is a nested dictionary between <script>
-d = parser.find(id="react-root")
-s = parser.find_all('script')
-scripts = s[12]
-# Isolates the nested dictionary
-jsonStr = re.search(r'\{.*\}', str(scripts)).group()
+# Transform: Parses the data for the information needed
+def transform(parser):
+    # Isolates the breeder, breed and litter information from the html which is a nested dictionary between <script>
+    s = parser.find_all('script')
+    # Selects the 12th script element
+    scripts = s[12]
+    # Isolates the nested dictionary
+    jsonStr = re.search(r'\{.*\}', str(scripts)).group()
 
-# Reformat the dictionary to a python friendly list of dictionary entries
-dct = json.loads(jsonStr)
-entries = dct['app']['search_results']['pages']['data']
+    # Reformat the dictionary to a python friendly list of dictionary entries
+    dct = json.loads(jsonStr)
+    entries = dct['app']['search_results']['pages']['data']
 
+    for e in entries:
+        list_entries.append(e)
+    return
+
+# Calls the transform function to get the parsed information, loads it into a dataframe and saves it as a .csv
 list_entries = []
-for e in entries:
-    list_entries.append(e)
+# gets pages 1 to 10
+for i in range(1, 10):
+    print(f'Getting page, {i}')
+    c = extract(i)
+    t = transform(c)
 
-# Converts the list into a Pandas Dataframe
 akc = pd.DataFrame(data=list_entries)
-print(akc)
-
-# Explores the number of rows and columns and prints the column titles
-akc.shape
-print(akc.columns)
-
 # Export File to CSV
 akc.to_csv('akc.csv')
 
-#
-#
-# #
-# # # Press the green button in the gutter to run the script.
-# # if __name__ == '__main__':
-# #     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
